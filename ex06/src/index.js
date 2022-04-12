d0f802427a6c03ed1eeb894072480c56daa9f233
Sumedin Bilalic <sumko87@gmail.com>
2022-04-12 23:54:54 +0200

    more JS and export

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+//first name upper
+function firstName (firstName) {
+    return firstName.toUpperCase();
+}
+exports.firstname =firstName;
+console.log(firstName("Sumi"));
+
+//last name lower
+function lastName (lastName) {
+    return lastName.toLowerCase();
+}
+exports.lastName =lastName;
+console.log(lastName("Sumi"));
